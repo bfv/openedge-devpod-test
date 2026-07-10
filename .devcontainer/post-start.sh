269396fec9882a27d2f1b845a498a8cdce1054d6
Bronco Oostermeyer <dev@bfv.io>
2026-07-10 13:51:34 +0000

    prevent commit of mcp.json

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+echo "post start script executing"
+
+echo "git update-index --skip-worktree .vscode/mcp.json"
+git update-index --skip-worktree .vscode/mcp.json
+
 echo "post start script executed"
